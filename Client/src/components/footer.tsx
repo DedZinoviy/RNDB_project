74807698d0192644d7021b9ce6b1f526cf05f62b
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2024-11-30 11:45:27 +0300

    Добавлен компонент футера сайта.

---
+++ Client/src/components/footer.tsx
@@ -0,0 +1,9 @@
+import './footer.css';
+
+export default function Footer() {
+    return (
+        <footer className='footer'>
+            <p>Все права защищены &copy; 2024</p>
+        </footer>
+    );
+}
