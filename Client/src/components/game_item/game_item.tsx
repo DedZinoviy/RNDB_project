aa3e387b6e40245bc9cf556e3c3689c8c7339ac7
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2024-11-30 14:48:21 +0300

    Добавлен компонент карточки игры.

---
+++ Client/src/components/game_item/game_item.tsx
@@ -0,0 +1,18 @@
+import { Game } from "../../types/game";
+
+interface GameItemProp {
+    game: Game;
+}
+
+export default function GameItem({game}: GameItemProp) {
+    const labelId = `list-label-${game.sid}`;
+    return (
+        <div className="game-item" id={labelId}>
+            <div className="game-info">
+                <h3>{game.name}</h3>
+                <p className="description">{game.description}</p>
+                <p className="price">Цена: {game.current_price}$</p>
+            </div>
+        </div>
+    );
+}
