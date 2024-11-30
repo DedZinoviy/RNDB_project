83c7df25b3b6765c4d463d0dc810ea391b8ee79c
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2024-11-30 14:20:15 +0300

    Добавлен компонент страницы списка игр.

---
+++ Client/src/pages/GameListPage.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/header/header";
+import Footer from "../components/footer/footer";
+import Filter from "../components/filter/filter";
+
+export default function GameListPage() {
+    return (
+        <>
+            <Header></Header>
+            <Filter></Filter>
+            <Footer></Footer>
+        </>
+    );
+}
