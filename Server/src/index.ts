1082127fbeb4e16e282484b85d5ca267f40c4013
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2024-12-09 14:03:23 +0300

    Добавлена настройка CORS для клиента.

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,9 +1,20 @@
 import express from 'express';
 import { SERVER_PORT } from './env/env-vars';
 import games_router from './routes/games_routes';
+import cors from 'cors';
 
 // Экземпляр серверного приложения.
 export const app = express();
+
+// Разрешить доступ с клиента.
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Разрешенные методы
+    allowedHeaders: ['Content-Type'], // Разрешенные заголовки
+}));
+
+// Обработка запросов с любым путем.
+app.options("*", cors());
 
 // Настройка обработки (парсинга) JSON.
 app.use(express.json());
