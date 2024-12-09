1145c7dedbaaa0ba048dd20ba16df0cc5d533818
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2024-12-09 17:22:30 +0300

    Запросы адаптированы под игры.

--- Client/src/requests/requests.ts
+++ Client/src/requests/requests.ts
@@ -3,20 +3,20 @@ import { FilterParams } from "../types/FilterParams";
 /** Адрес сервера. */
 const host = "localhost:5000"
 
-/** Получить все фильмы от сервера 
+/** Получить все игры от сервера 
  * @returns ответ от сервера.
 */
-export async function get_all_films() {
+export async function get_all_games() {
     const response = await fetch('http://'+ host + '/games', {method: 'GET'}); // Отправить запрос на сервер.
     return response;
 }
 
 /**
- * Получить фильмы от сервера с учетом параметров запроса.
+ * Получить игры от сервера с учетом параметров запроса.
  * @param params параметры запроса, полученные из фильтров.
  * @returns ответ от сервера.
  */
-export async function get_films_by(params: FilterParams) {
+export async function get_games_by(params: FilterParams) {
     const checked_params: { [key: string]: string } = {};
     // Добавить минимальную текущую цену, если таковая определена.
     if (params.minCurPrice !== undefined && !Number.isNaN(params.minCurPrice)) {checked_params.min_cur_price = params.minCurPrice.toString();}
