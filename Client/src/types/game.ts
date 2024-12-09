6e9fde1fc872dbd945bc33d8cc5cfef706f917d3
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2024-12-09 17:28:19 +0300

    Добавлена функция перевода JSON массива игр с сервера к массиву определяемого типа.

--- Client/src/types/game.ts
+++ Client/src/types/game.ts
@@ -36,3 +36,29 @@ export type Game = {
     /** Рейтинг игры на IGDB. */
     igdb_score: number;
 }
+
+/**
+ * Перевести результат запроса на сервер в массив игр с указанным типом.
+ * @param json_array Массив, полученный от сервера.
+ * @returns Массив игр.
+ */
+export function json_to_game(json_array: {[key: string]: string }[]): Game[] {
+    const games: Game[] = []; // Считать, что изначальный массив игр пустой.
+    for (const elem of json_array) { // Для каждой игры из ответа сервера...
+        // Создать объект игры, эквивалентный записи на сервере.
+        const game: Game = {sid: Number(elem.sid),
+                            store_url: elem.store_url,
+                            published: elem.published_store,
+                            name: elem.name,
+                            image: elem.image,
+                            description: elem.description,
+                            current_price: Number(elem.current_price),
+                            developers: elem.developers,
+                            publishers: elem.publishers,
+                            genres: elem.genres,
+                            igdb_score: Number(elem.igdb_score)
+        };
+        games.push(game); // Добавить игру в изначальный массив.
+    }
+    return games;
+}
