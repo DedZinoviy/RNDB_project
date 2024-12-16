8b9ec7b51d1fe8126678774e314310f47f64b1d0
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2024-12-16 13:00:12 +0300

    Добавлен метод получения списка уникальных жанров в отображении.

--- Client/src/types/game.ts
+++ Client/src/types/game.ts
@@ -62,3 +62,21 @@ export function json_to_game(json_array: {[key: string]: string }[]): Game[] {
     }
     return games;
 }
+
+/**
+ * Получить массив уникальных жанров игр.
+ * @param games массив игр с жанрами.
+ * @returns массив уникальных элементов, состоящий из жанров.
+ */
+export function get_unique_genres(games: Game[]) : string[] {
+    const unique_genres: string[] = []; // Считать, что изначально массив жанров пуст.
+    for (const game of games) { // Для каждой игры массива...
+        const genres = game.genres.split(','); // Получить список жанров игры.
+        for (const genre of genres) { // Для каждого жанра игры...
+            if (!unique_genres.includes(genre)) { // Считать жанр уникальным, если он отсуствует в массиве уникальных жанров.
+                unique_genres.push(genre); 
+            }
+        }
+    }
+    return unique_genres;
+}
